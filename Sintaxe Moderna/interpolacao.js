7e6df2d03ff2b558c4b6c3160e92823bdc5593ac
byKoro <yuriludwig.email@gmail.com>
2026-04-29 17:05:10 -0300

    estudei interpolacao com crase

---
+++ Sintaxe Moderna/interpolacao.js
@@ -0,0 +1,8 @@
+const usuario = {
+  nome: 'Yuri',
+  idade: 22,
+  cidade: 'Campo Grande',
+};
+const { nome, idade, cidade } = usuario;
+
+console.warn(`Meu nome é ${nome}, tenho ${idade} anos e moro em ${cidade}`);
